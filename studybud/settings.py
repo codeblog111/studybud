e1e7211ef57758c09e86714cab0f228c7401eca9
Hassan Hamed <hassanhamedd1234@gmail.com>
2022-05-24 01:55:06 +0300

    Modified MEDIA_ROOT directory
    there was an '/' instead of ',' in MEDIA_ROOT directory

--- studybud/settings.py
+++ studybud/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR / 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # STATIC_ROOT =
 
